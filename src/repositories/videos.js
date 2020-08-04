5ea3e0cd848d9dd81fd00dfdc2bd16b19ea34c71
Balthyer <eduardo.afoli@gmail.com>
2020-08-04 19:11:21 -0300

    fix back-end

--- src/repositories/videos.js
+++ src/repositories/videos.js
@@ -5,7 +5,7 @@ const URL_VIDEOS = `${config.URL_BACKEND}/videos`;
 
 function createVideo(videoData) {
   return axios.post(URL_VIDEOS, {
-    videoData,
+    ...videoData,
   })
     .then((response) => {
       // eslint-disable-next-line no-console
